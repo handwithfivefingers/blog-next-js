0eecdf7712872d5c6ffdb259c0b92a5020e5d953
MAI VAN TRUYEN <truyenmv@ominext.com>
2021-10-02 23:34:20 +0700

    -

--- apollo-client.js
+++ apollo-client.js
@@ -3,6 +3,7 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 const client = new ApolloClient({
   uri: `${process.env.NEXT_PUBLIC_WORDPRESS_API_URL}`,
   cache: new InMemoryCache(),
+  
 });
 
 export default client;
