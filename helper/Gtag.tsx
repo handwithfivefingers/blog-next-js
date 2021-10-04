2f099b1896894a6cc356f38eb382bcbf8be794c4
MAI VAN TRUYEN <truyenmv@ominext.com>
2021-10-04 10:35:26 +0700

    change GA-trackking id

--- helper/Gtag.tsx
+++ helper/Gtag.tsx
@@ -1,8 +1,9 @@
-export const GA_TRACKING_ID = "RP5GM0wMRHOPj6ICk3myPQ";
+// export const GA_TRACKING_ID = "RP5GM0wMRHOPj6ICk3myPQ";
+export const GA_TRACKING_ID = 'G-E2045P5WG5';
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export const pageview = (url: URL): void => {
-  window.gtag("config", GA_TRACKING_ID, {
+  window.gtag('config', GA_TRACKING_ID, {
     page_path: url,
   });
 };
@@ -16,7 +17,7 @@ type GTagEvent = {
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/events
 export const event = ({ action, category, label, value }: GTagEvent): void => {
-  window.gtag("event", action, {
+  window.gtag('event', action, {
     event_category: category,
     event_label: label,
     value,
